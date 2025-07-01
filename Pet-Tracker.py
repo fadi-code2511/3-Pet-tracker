f655b5ce7944371c187f66f449117a3f64ef8bb3
Fadi nasr <nasr.fadi2511@gmail.com>
2025-07-01 13:53:26 +0300

    Create a list of pets manually

--- Pet-Tracker.py
+++ Pet-Tracker.py
@@ -30,24 +30,26 @@ class Pet:
     def birthday(self):
         self.age+=1
 
+# Part 2:
+# Create a list of pets manually.
+# Write a function that finds all non-adopted pets.
+# Add a method rename(new_name) that changes the name of the pet.
+pets=[Pet("Jeje","cat",5),Pet("Wees","duck",1),Pet("Wezo","rabbit",4)]
 
 
 # for testing purpose
-pet1 = Pet("sally", "cat", 3)
-pet1.display_info()
-pet1.mark_adopted()
-pet1.birthday()
-pet1.display_info()
-
-
+# pet1 = Pet("Sally", "cat", 3)
+# pet1.display_info()
+# pet1.mark_adopted()
+# pet1.birthday()
 # pet1.display_info()
 
 # pet2 = Pet("Max", "dog", 5, adopted=True)
 # pet2.display_info()
 
-pet3=Pet("toto","monkey",4)
-pet3.display_info()
-pet3.mark_adopted()
-pet3.birthday()
-pet3.display_info()
+# pet3=Pet("Toto","monkey",4)
+# pet3.display_info()
+# pet3.mark_adopted()
+# pet3.birthday()
+# pet3.display_info()
 
